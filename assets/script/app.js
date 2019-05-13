05260d06bb8dfc46f733b13ac5c4ce8884f7d980
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-05-13 11:40:35 -0400

    mobile nav

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.scrollspy').scrollSpy();
 });
 
@@ -6,7 +6,20 @@ skrollr.init({
     forceHeight: false
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
-  });
-        
+});
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById('small-nav').style.top = "0";
+    } else {
+        document.getElementById("small-nav").style.top = "-65px";
+    }
+}
+
+skrollr.init({
+    forceHeight: false
+});
