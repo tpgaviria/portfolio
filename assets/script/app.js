5e360d49cda68baeb7bd1f9f6d37aab153af54b2
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-06-27 10:43:17 -0400

    wip

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,25 +1,41 @@
-$(document).ready(function () {
-    $('.scrollspy').scrollSpy();
-});
+// $(document).ready(function(){
+//     $('body').show();
+// })
 
-skrollr.init({
-    forceHeight: false
-});
+var vw = $(window).width();
+console.log(vw);
 
-$(document).ready(function () {
-    $('.sidenav').sidenav();
-});
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-        document.getElementById('small-nav').style.top = "0";
-    } else {
-        document.getElementById("small-nav").style.top = "-65px";
-    }
+if (vw < 995) {
+    console.log('small vw')
+    AOS.init({
+        offset: 200, // offset (in px) from the original trigger point
+        delay: 100, // values from 0 to 3000, with step 50ms
+        duration: 900, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'center-center' // defines which position of the element regarding to window should trigger the animation
+    });
+} else {
+    console.log('larger vw');
+    AOS.init({
+        offset: 500, // offset (in px) from the original trigger point
+        delay: 100, // values from 0 to 3000, with step 50ms
+        duration: 900, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-top' // defines which position of the element regarding to window should trigger the animation
+    });
 }
 
-skrollr.init({
-    forceHeight: false
+var typed = new Typed("#typed", {
+    stringsElement: '#typed-strings',
+    typeSpeed: 40,
+    backSpeed: 40,
+    backDelay: 1200,
+    startDelay: 500,
+    loop: false,
+    showCursor: true
 });
